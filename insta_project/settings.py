f8b900cead241f05652d1fdfb5ddabe2d9bda9a1
kobiyoyo <adamachubi@gmail.com>
2020-04-12 23:33:08 +0100

    added git ignore

--- insta_project/settings.py
+++ insta_project/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'insta_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'scraper',
+        'USER': '',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
