8322fd0bb720ed16a1774ce0d4caf9c7898b102b
kobiyoyo <adamachubi@gmail.com>
2020-04-13 01:12:51 +0100

    updated settings

--- insta_project/settings.py
+++ insta_project/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&rh8lh_2@e+v^clqw225tcqa6y6&n+@5s-*u&n9k#7+ita9)lr'
-
+# SECRET_KEY = '&rh8lh_2@e+v^clqw225tcqa6y6&n+@5s-*u&n9k#7+ita9)lr'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['honeyscrap.herokuapp.com']
 
 
 # Application definition
