02e3edc3aa3a140af0ef36cb11d483773ecf690d
kobiyoyo <adamachubi@gmail.com>
2020-04-13 10:14:49 +0100

    added django_heroku to settings

--- insta_project/settings.py
+++ insta_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -130,3 +131,7 @@ MEDIA_URL = '/media/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'post-list'
 LOGIN_URL = 'login'
+
+django_heroku.settings(locals())
+
+
